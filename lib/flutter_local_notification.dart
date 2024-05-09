d7e20ad354edef81ae69058e324375b85d65c63d
Mahmoud Shaaban <cumody82@gmail.com>
2024-05-09 13:48:56 +0300

    chore: Update Android dependencies and manifest permissions

---
+++ lib/flutter_local_notification.dart
@@ -0,0 +1,58 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotificationService {
+  static FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  static onTap(NotificationResponse notificationResponse) {
+    // log(notificationResponse.id!.toString());
+    // log(notificationResponse.payload!.toString());
+    // Navigator.push(context, route);
+  }
+
+  static Future init() async {
+    InitializationSettings settings = const InitializationSettings(
+      android: AndroidInitializationSettings('@mipmap/ic_launcher'),
+      iOS: DarwinInitializationSettings(),
+    );
+    flutterLocalNotificationsPlugin.initialize(
+      settings,
+      onDidReceiveNotificationResponse: onTap,
+      onDidReceiveBackgroundNotificationResponse: onTap,
+    );
+  }
+
+  //basic Notification
+  static int notificationId = 0;
+  static void showBasicNotification() async {
+    AndroidNotificationDetails android = const AndroidNotificationDetails(
+        'id_1', 'Basic Notification',
+        importance: Importance.max, priority: Priority.max, playSound: true);
+    NotificationDetails details = NotificationDetails(android: android);
+    await flutterLocalNotificationsPlugin.show(
+      notificationId++, // Increment the ID each time
+      'Basic Notification',
+      'This is the notification body.',
+      details,
+    );
+  }
+
+  //basic Notification2
+  static void showBasicNotification2() async {
+    AndroidNotificationDetails android = AndroidNotificationDetails(
+      'id 3',
+      'basic notification1',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+    NotificationDetails details = NotificationDetails(
+      android: android,
+    );
+    await flutterLocalNotificationsPlugin.show(
+      4,
+      'Basic Notification',
+      'body',
+      details,
+    );
+  }
+}
